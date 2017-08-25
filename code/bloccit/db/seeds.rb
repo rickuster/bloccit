21b7a7b92a3be4d759235c51c9e005a334e8ec8c
rickuster <rickuster@gmail.com>
2017-08-24 23:10:11 -0700

    Completed rails seeding data assignment

--- code/bloccit/db/seeds.rb
+++ code/bloccit/db/seeds.rb
@@ -19,6 +19,12 @@ posts = Post.all
   )
 end
 
+#Create unique post if one does not already exists
+uniquepost = Post.find_or_create_by(title: "This is a unique title", body: "This is a unique body")
+
+#Create unique comment if one does not already exists
+uniquepost.comments.find_or_create_by(body: "This is a unique body")
+
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
