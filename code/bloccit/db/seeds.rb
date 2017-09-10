71595a82ef27c535297ef8ce7c062db677591d38
rickuster <rickuster@gmail.com>
2017-09-09 22:15:12 -0700

    Completed rails crud assignment

--- code/bloccit/db/seeds.rb
+++ code/bloccit/db/seeds.rb
@@ -19,6 +19,16 @@ posts = Post.all
   )
 end
 
+#Create Advertisements
+50.times do
+  Advertisement.create!(
+    title: RandomData.random_sentence,
+    body:  RandomData.random_paragraph,
+    price: RandomData.random_integer
+  )
+end
+
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+puts "#{Advertisement.count} Advertisements created"
--- code/bloccit/lib/random_data.rb
+++ code/bloccit/lib/random_data.rb
@@ -26,4 +26,8 @@ module RandomData
     letters[0, rand(3..8)].join
   end
 
+  def self.random_integer
+    integers = rand(0..1000)
+  end
+
 end
