564ef83cbb2e1de2b0c762029bd3c1bb0ef2c1c8
rickuster <rickuster@gmail.com>
2017-09-14 22:26:54 -0700

    Completed more crud assignment

--- code/bloccit/db/seeds.rb
+++ code/bloccit/db/seeds.rb
@@ -7,9 +7,14 @@ require 'random_data'
     title: RandomData.random_sentence,
     body:  RandomData.random_paragraph
   )
+  Question.create!(
+    title: RandomData.random_sentence,
+    body:  RandomData.random_paragraph
+  )
 end
 
 posts = Post.all
+questions = Question.all
 
 #Create comments
 100.times do
@@ -22,3 +27,4 @@ end
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+puts "#{Question.count} questions created"
--- code/bloccit/spec/controllers/posts_controller_spec.rb
+++ code/bloccit/spec/controllers/posts_controller_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe PostsController, type: :controller do
     it "assigns [my_post] to @posts" do
       get :index
 
-      expect(assigns(:posts)).to_eq([my_post])
+      expect(assigns(:posts)).to eq([my_post])
     end
   end
 
