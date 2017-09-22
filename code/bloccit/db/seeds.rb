fa3f2a97846aaaeb2229ca1bb8f9fb1c038eb375
rickuster <rickuster@gmail.com>
2017-09-21 23:44:52 -0700

    Completed rails topics and posts

--- code/bloccit/db/seeds.rb
+++ code/bloccit/db/seeds.rb
@@ -1,9 +1,19 @@
 require 'random_data'
 
+15.times do
+  Topic.create!(
+    name:   RandomData.random_sentence,
+    description: RandomData.random_paragraph
+  )
+end
+
+topics = Topic.all
+
 #Create posts
 50.times do
   #add fail warning to create
   Post.create!(
+    topic: topics.sample,
     title: RandomData.random_sentence,
     body:  RandomData.random_paragraph
   )
@@ -20,5 +30,6 @@ posts = Post.all
 end
 
 puts "Seed finished"
+puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
