323a297ecfabb5254a03e05e6405794456ef150f
rickuster <rickuster@gmail.com>
2017-09-29 00:14:15 -0700

    Completed auth signing up assignment

--- code/bloccit/config/routes.rb
+++ code/bloccit/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     resources :posts, except: [:index]
   end
   resources :users, only: [:new, :create]
+  post 'users/confirm' => 'users#confirm'
   get 'about' => 'welcome#about'
   root 'welcome#index'
 end
