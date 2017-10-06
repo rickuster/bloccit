6b86cb4b086dba0d0277bb3b4f0dd5b942ecaec9
rickuster <rickuster@gmail.com>
2017-10-05 22:20:48 -0700

    Completed auth signing in assignment

--- code/bloccit/app/helpers/application_helper.rb
+++ code/bloccit/app/helpers/application_helper.rb
@@ -5,4 +5,10 @@ module ApplicationHelper
 
     content_tag :div, capture(&block), class: css_class
   end
+
+  def avatar_url(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    gravatar_url = "http://gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
 end
