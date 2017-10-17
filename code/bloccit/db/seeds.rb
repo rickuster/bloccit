51e958a4d2d8dfb2ba2fac0840a96e58c96b30da
rickuster <rickuster@gmail.com>
2017-10-16 22:36:37 -0700

    Completed rails posts and users

--- code/bloccit/db/seeds.rb
+++ code/bloccit/db/seeds.rb
@@ -1,5 +1,15 @@
 require 'random_data'
 
+# Create Users
+5.times do
+  User.create!(
+    name: RandomData.random_name,
+    email: RandomData.random_email,
+    password: RandomData.random_sentence
+  )
+end
+users = User.all
+
 15.times do
   Topic.create!(
     name:   RandomData.random_sentence,
@@ -13,6 +23,7 @@ topics = Topic.all
 50.times do
   #add fail warning to create
   Post.create!(
+    user: users.sample,
     topic: topics.sample,
     title: RandomData.random_sentence,
     body:  RandomData.random_paragraph
@@ -29,7 +40,14 @@ posts = Post.all
   )
 end
 
+user = User.first
+user.update_attributes!(
+  email: 'rickuster@gmail.com',
+  password: 'helloworld'
+)
+
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
