5831b66ae8a36f53fee1dab78938eefa5713f413
rickuster <rickuster@gmail.com>
2017-10-23 00:07:25 -0700

    Completed git rails voting assignment

--- code/bloccit/app/controllers/posts_controller.rb
+++ code/bloccit/app/controllers/posts_controller.rb
@@ -61,7 +61,7 @@ before_action :authorize_user, except: [:show, :new, :create]
   end
 
   def authorize_user
-  post = Post.find(params[:id])
+    post = Post.find(params[:id])
     unless current_user == post.user || current_user.admin?
       flash[:alert] = "You must be an admin to do that."
       redirect_to [post.topic, post]
