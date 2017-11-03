eabd45e49d970cdbb88081037cc878e373806f83
rickuster <rickuster@gmail.com>
2017-11-02 20:56:39 -0700

    Completed rails public profile assignment

--- code/bloccit/app/helpers/users_helper.rb
+++ code/bloccit/app/helpers/users_helper.rb
@@ -1,2 +1,17 @@
 module UsersHelper
+  def check_and_list_posts(user)
+    if @user.posts.empty?
+      "#{user.name} has not submitted any posts yet."
+    else
+      render @user.posts
+    end
+  end
+
+  def check_and_list_comments(user)
+    if @user.comments.empty?
+      "#{user.name} has not submitted any comments yet."
+    else
+      render @user.comments
+    end
+  end
 end
